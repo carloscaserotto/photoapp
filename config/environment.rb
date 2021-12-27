51f9159df6b9ec24d77117392eb3ab9051676332
carlos <carlos.caserotto@yahoo.com>
2021-12-27 13:37:16 -0300

    Setup email functionality for signup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => Rails.application.credentials.email_info[:email],
+    :password             => Rails.application.credentials.email_info[:password],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,12 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://chino-photo-app.herokuapp.com/' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
+
+  
+
+ 
 end
